9d00616311a9b84572bc0ffde424cb8828c56504
Valentin Boettcher <valentin@boettcher.cf>
2020-04-10 14:51:26 +0200

    describe integration with vegas

--- prog/python/qqgg/tangled/plot_utils.py
+++ prog/python/qqgg/tangled/plot_utils.py
@@ -1,25 +1,3 @@
-"""
-Some shorthands for common plotting tasks related to the investigation
-of monte-carlo methods in one rimension.
-
-Author: Valentin Boettcher <hiro at protagon.space>
-"""
-
-import matplotlib.pyplot as plt
-
-def draw_histo(points, xlabel, bins=20):
-    heights, edges = np.histogram(points, bins)
-    centers = (edges[1:] + edges[:-1])/2
-    deviations = np.sqrt(heights)
-
-    fig, ax = set_up_plot()
-    ax.errorbar(centers, heights, deviations, linestyle='none', color='orange')
-    ax.step(edges,  [heights[0], *heights], color='#1f77b4')
-
-    ax.set_xlabel(xlabel)
-    ax.set_xlim([points.min(), points.max()])
-    return fig, ax
-
 def plot_increments(ax, increment_borders, label=None, *args, **kwargs):
     """Plot the increment borders from a list.  The first and last one
 
@@ -30,7 +8,7 @@ def plot_increments(ax, increment_borders, label=None, *args, **kwargs):
 
     ax.axvline(x=increment_borders[1], label=label, *args, **kwargs)
 
-    for increment in increment_borders[2:-1]:
+    for increment in increment_borders[1:-1]:
         ax.axvline(x=increment, *args, **kwargs)
 
 def plot_vegas_weighted_distribution(ax, points, dist,
@@ -53,3 +31,25 @@ def plot_vegas_weighted_distribution(ax, points, dist,
         weighted_dist[mask] = dist[mask]*num_increments*length
 
     ax.plot(points, weighted_dist, *args, **kwargs)
+
+"""
+Some shorthands for common plotting tasks related to the investigation
+of monte-carlo methods in one rimension.
+
+Author: Valentin Boettcher <hiro at protagon.space>
+"""
+
+import matplotlib.pyplot as plt
+
+def draw_histo(points, xlabel, bins=20):
+    heights, edges = np.histogram(points, bins)
+    centers = (edges[1:] + edges[:-1])/2
+    deviations = np.sqrt(heights)
+
+    fig, ax = set_up_plot()
+    ax.errorbar(centers, heights, deviations, linestyle='none', color='orange')
+    ax.step(edges,  [heights[0], *heights], color='#1f77b4')
+
+    ax.set_xlabel(xlabel)
+    ax.set_xlim([points.min(), points.max()])
+    return fig, ax
