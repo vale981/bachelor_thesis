412dd1c6956bbe84bc31ee80ae2f0916aa947637
Valentin Boettcher <valentin@boettcher.cf>
2020-04-14 16:57:10 +0200

    finish observables

--- prog/python/qqgg/tangled/plot_utils.py
+++ prog/python/qqgg/tangled/plot_utils.py
@@ -62,15 +62,17 @@ Author: Valentin Boettcher <hiro at protagon.space>
 
 import matplotlib.pyplot as plt
 
+
 def draw_histo(points, xlabel, bins=20):
     heights, edges = np.histogram(points, bins)
-    centers = (edges[1:] + edges[:-1])/2
+    centers = (edges[1:] + edges[:-1]) / 2
     deviations = np.sqrt(heights)
 
     fig, ax = set_up_plot()
-    ax.errorbar(centers, heights, deviations, linestyle='none', color='orange')
-    ax.step(edges,  [heights[0], *heights], color='#1f77b4')
+    ax.errorbar(centers, heights, deviations, linestyle="none", color="orange")
+    ax.step(edges, [heights[0], *heights], color="#1f77b4")
 
     ax.set_xlabel(xlabel)
+    ax.set_ylabel("Count")
     ax.set_xlim([points.min(), points.max()])
     return fig, ax
