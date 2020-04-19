97106f6c20f4d7c15d4decba1ca9f29e04a0c180
hiro98 <hiro@protagon.space>
2020-04-19 16:05:58 +0200

    the pdf stuff seems to work, but needs Verification

--- prog/python/qqgg/tangled/pdf.py
+++ prog/python/qqgg/tangled/pdf.py
@@ -9,6 +9,7 @@ import numpy as np
 import monte_carlo
 import lhapdf
 
+
 def energy_factor(e_proton, charge, x_1, x_2):
     """Calculates the factor common to all other values in this module.
 
@@ -20,24 +21,62 @@ def energy_factor(e_proton, charge, x_1, x_2):
     """
     return charge ** 4 / (137.036 * e_proton) ** 2 / (24 * x_1 * x_2)
 
+
 def momenta(e_proton, x_1, x_2, cosθ):
     """Given the Energy of the incoming protons `e_proton` and the
     momentum fractions `x_1` and `x_2` as well as the cosine of the
     azimuth angle of the first photon the 4-momenta of all particles
     are calculated.
     """
-    q_1 = e_proton * x_1 * np.array([1, 0, 0, 1])
-    q_2 = e_proton * x_2 * np.array([1, 0, 0, -1])
-    g_3 = (
+    x_1 = np.asarray(x_1)
+    x_2 = np.asarray(x_2)
+    cosθ = np.asarray(cosθ)
+    assert (
+        x_1.shape == x_2.shape == cosθ.shape
+    ), "Invalid shapes for the event parameters."
+
+    q_1 = (
         e_proton
+        * x_1
+        * np.array(
+            [
+                np.ones_like(cosθ),
+                np.zeros_like(cosθ),
+                np.zeros_like(cosθ),
+                np.ones_like(cosθ),
+            ]
+        )
+    )
+    q_2 = (
+        e_proton
+        * x_2
+        * np.array(
+            [
+                np.ones_like(cosθ),
+                np.zeros_like(cosθ),
+                np.zeros_like(cosθ),
+                -np.ones_like(cosθ),
+            ]
+        )
+    )
+    g_3 = (
+        2
+        * e_proton
         * x_1
         * x_2
         / (2 * x_2 + (x_1 - x_2) * (1 - cosθ))
-        * np.array([1, cosθ, 0, np.sqrt(1-cosθ**2)])
+        * np.array(
+            [1 * np.ones_like(cosθ), np.sqrt(1 - cosθ ** 2), np.zeros_like(cosθ), cosθ]
+        )
     )
     g_4 = q_1 + q_2 - g_3
 
-    return q_1, q_2, g_3, g_4
+    q_1 = q_1.reshape(4, cosθ.size).T
+    q_2 = q_2.reshape(4, cosθ.size).T
+    g_3 = g_3.reshape(4, cosθ.size).T
+    g_4 = g_4.reshape(4, cosθ.size).T
+
+    return np.array([q_1, q_2, g_3, g_4])
 
 
 def diff_xs(e_proton, charge, cosθ, x_1, x_2):
@@ -60,19 +99,38 @@ def diff_xs(e_proton, charge, cosθ, x_1, x_2):
         (1 - cosθ ** 2) * (x_1 * (1 - cosθ) + x_2 * (1 + cosθ))
     )
 
-def t_channel_q2(e_proton, cosθ, x_1, x_2):
-    p, _, p_tag, _ = momenta(e_proton, x_1, x_2, cosθ)
-    q = (p - p_tag)
 
-    return -minkowski_product(q, q)
+def diff_xs_η(e_proton, charge, η, x_1, x_2):
+    """Calculates the differential cross section as a function of the
+    cosine of the pseudo rapidity η of one photon in units of 1/GeV².
+
+    Here dΩ=dηdφ
+
+    :param e_proton: proton energy per beam [GeV]
+    :param charge: charge of the quark
+    :param x_1: momentum fraction of the first quark
+    :param x_2: momentum fraction of the second quark
+    :param η: pseudo rapidity
+
+    :return: the differential cross section [GeV^{-2}]
+    """
+    tanh_η = np.tanh(η)
+    f = energy_factor(e_proton, charge, x_1, x_2)
+
+    return (
+        (x_1 ** 2 * (1 - tanh_η) ** 2 + x_2 ** 2 * (1 + tanh_η) ** 2)
+        / ((1 - tanh_η ** 2) * (x_1 * (1 - tanh_η) + x_2 * (1 + tanh_η)))
+        * (1 - tanh_η ** 2)
+    )
 
 def get_xs_distribution_with_pdf(xs, q, e_hadron, quarks=None, pdf=None):
     """Creates a function that takes an event (type np.ndarray) of the
-    form [cosθ, impulse fractions of quarks in hadron
-    1, impulse fractions of quarks in hadron 2] and returns the
-    differential cross section for such an event. I would have used an
-    object as argument, wasn't for the sampling function that needs a
-    vector valued function.
+    form [cosθ, impulse fractions of quarks in hadron 1, impulse
+    fractions of quarks in hadron 2] and returns the differential
+    cross section for such an event. I would have used an object as
+    argument, wasn't for the sampling function that needs a vector
+    valued function. Cosθ can actually be any angular-like parameter
+    as long as the xs has the corresponding parameter.
 
     :param xs: cross section function with signature (energy hadron, cosθ, x_1, x_2)
     :param q2: the momentum transfer Q^2 as a function with the signature
@@ -113,11 +171,9 @@ def get_xs_distribution_with_pdf(xs, q, e_hadron, quarks=None, pdf=None):
 
     return distribution, (pdf.xMin, pdf.xMax)
 
-def sample_momenta(num_samples, dist, interval, e_hadron):
-    cosθ, x_1, x_2 = monte_carlo.sample_unweighted_array(
-        num_samples, dist, interval
-    ).T
-
-    print(cosθ, x_1, x_2)
-
-    return momenta(e_hadron, x_1, x_2, cosθ)[2:]  # only final state...
+def sample_momenta(num_samples, dist, interval, e_hadron, upper_bound=None):
+    res, eff = monte_carlo.sample_unweighted_array(
+        num_samples, dist, interval, upper_bound=upper_bound, report_efficiency=True
+    )
+    cosθ, x_1, x_2 = res.T
+    return momenta(e_hadron, x_1[None, :], x_2[None, :], cosθ[None, :]), eff
