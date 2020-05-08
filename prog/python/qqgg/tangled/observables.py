3a811cdd70fc70a0d3cbd89371b708a61f61fdc8
hiro98 <hiro@protagon.space>
2020-05-08 15:50:33 +0200

    add some more histos

--- prog/python/qqgg/tangled/observables.py
+++ prog/python/qqgg/tangled/observables.py
@@ -1,5 +1,6 @@
 """This module defines some observables on arrays of 4-pulses."""
 import numpy as np
+from utility import minkowski_product
 
 def p_t(p):
     """Transverse momentum
@@ -16,3 +17,16 @@ def η(p):
     """
 
     return np.arccosh(np.linalg.norm(p[:,1:], axis=1)/p_t(p))*np.sign(p[:, 3])
+
+def inv_m(p_1, p_2):
+    """Invariant mass off the final state system.
+
+    :param p_1: array of 4-momenta, first fs particle
+    :param p_2: array of 4-momenta, second fs particle
+    """
+
+    total_p = p_1 + p_2
+    return np.sqrt(minkowski_product(total_p, total_p))
+
+def cosθ(p):
+    return p[:, 3] / p[:, 0]
